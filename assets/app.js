11d106f7b5d0099090d6568ec815366d23a1be09
jihane <jihane.harimech@gmail.com>
2023-03-06 18:30:06 +0100

    add dashboard template

--- assets/app.js
+++ assets/app.js
@@ -7,12 +7,23 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import './styles/custom.scss';
+import './css/sb-admin-2.min.css';
+import './css/sb-admin-2.css';
+import './js/sb-admin-2.min.js';
+import './js/sb-admin-2.js';
+
 
 // start the Stimulus application
 import './bootstrap';
+import 'bootstrap';
+
 import $ from 'jquery';
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+
+
